ca2be94e9b8ef007d1807fd8b665179a2403dadf
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-20 11:39:16 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Hmm/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Hmm-Cy',
-    version='1.0.2',
+    version='1.0.3',
     packages=['Hmm'],
     package_data={'Hmm': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/Hmm-Cy',
